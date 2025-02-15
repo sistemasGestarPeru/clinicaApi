eea57b25069224f304ccbda1dc0d50bd78fedaa9
ReyesDemetrio <reyesdeme08@gmail.com>
2025-02-14 21:10:08 -0500

    falta arreglar anular en el back

--- src/app/Models/Recaudacion/Anulacion.php
+++ src/app/Models/Recaudacion/Anulacion.php
@@ -17,6 +17,7 @@ class Anulacion extends Model
         'Fecha',
         'CodigoDocumentoVenta',
         'CodigoMotivo',
-        'CodigoTrabajador'
+        'CodigoTrabajador',
+        'Comentario'
     ];
 }
