dfe5011d5c1c2b3358e9d3f73b214394c0c2df53
ReyesDemetrio <reyesdeme08@gmail.com>
2025-03-17 12:55:53 -0500

    Modelo Comision

--- src/app/Models/Recaudacion/Comision.php
+++ src/app/Models/Recaudacion/Comision.php
@@ -5,17 +5,24 @@ namespace App\Models\Recaudacion;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PagoComision extends Model
+class Comision extends Model
 {
     use HasFactory;
     public $timestamps = false;
 
-    protected $table = 'pagocomision';
+    protected $table = 'comision';
     protected $primaryKey = 'Codigo';
 
     protected $fillable = [
         'Codigo',
-        'CodigoMedico',
-        'Comentario',
+        'CodigoDocumentoVenta',
+        'CodigoContrato',
+        'CodigoPagoComision',
+        'Monto',
+        'TipoDocumento',
+        'Serie',
+        'Numero',
+        'Vigente',
+        'CodigoMedico'
     ];
 }
