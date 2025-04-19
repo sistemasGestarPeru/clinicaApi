789e6cf682fff9f4430edfdcc7770deece70b814
demetrio.reyes <demetrio.reyes@softtek.com>
2025-04-19 12:36:38 -0500

    Actualizar Compra / Falta q no tenga ingreso

--- src/resources/lang/es/mensajes.php
+++ src/resources/lang/es/mensajes.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'error_act_compra' => 'No se puede actualizar una Compra en estado Inactivo.',
     'error_act_egreso' => 'No se puede actualizar el pago :tipo en estado Inactivo.',
     'error_act_egreso_caja' => 'No se puede actualizar el pago :tipo. La caja registrada está cerrada.',
     'error_fecha_pago' => 'La fecha del pago de servicio no puede ser mayor a la fecha de apertura de caja.',
