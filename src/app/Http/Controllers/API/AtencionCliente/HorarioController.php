48600321d22235a3ab9b6f25c7c5db348751b547
demetrio.reyes <demetrio.reyes@softtek.com>
2025-05-04 17:24:33 -0500

    Fix consulta horario

--- src/app/Http/Controllers/API/AtencionCliente/HorarioController.php
+++ src/app/Http/Controllers/API/AtencionCliente/HorarioController.php
@@ -50,43 +50,15 @@ class HorarioController extends Controller
     }
 
 
-    // public function listarHorarios(){
-    //     try{
-    //         $horarios = Horario::all()->where('Vigente', 1);
-    //         return response()->json($horarios, 200);
-    //     }catch(\Exception $e){
-    //         return response()->json([
-    //             'msg' => 'Error al listar los horarios.',
-    //             'error' => $e->getMessage()
-    //         ], 500);
-    //     }
-    // }
-
-    public function listarHorarios(Request $request){
-        
+    public function listarHorarios(Request $request)
+    {
         try {
+            $horarios = Horario::select('Codigo', 'CodigoMedico', 'Fecha', 'HoraInicio', 'HoraFin')
+                ->where('Vigente', 1)
+                ->where('CodigoSede', $request->Sede)
+                ->get();
 
-            $query = Horario::where('Vigente', 1);
-            
-            // Filtro por médico (solo si no es 0)
-            if ($request->Medico != 0) {
-                $query->where('CodigoMedico', $request->Medico);
-            }
-            
-            // Filtro por sede
-            if ($request->Sede != 0) {
-                $query->where('CodigoSede', $request->Sede);
-            }
-            
-            // Filtro por rango de fechas
-            // if ($request->has('FechaInicio') && $request->has('FechaFin')) {
-            //     $query->whereBetween('Fecha', [$request->FechaInicio, $request->FechaFin]);
-            // }
-            
-            $horarios = $query->get();
-            
             return response()->json($horarios, 200);
-
         } catch (\Exception $e) {
             return response()->json([
                 'msg' => 'Error al listar los horarios.',
@@ -95,21 +67,56 @@ class HorarioController extends Controller
         }
     }
 
-    public function registrarHorario(Request $request){
 
-        try{
+    // public function listarHorarios(Request $request)
+    // {
+
+    //     try {
+
+    //         $query = Horario::where('Vigente', 1);
+
+    //         // Filtro por médico (solo si no es 0)
+    //         if ($request->Medico != 0) {
+    //             $query->where('CodigoMedico', $request->Medico);
+    //         }
+
+    //         // Filtro por sede
+    //         if ($request->Sede != 0) {
+    //             $query->where('CodigoSede', $request->Sede);
+    //         }
+
+    //         // Filtro por rango de fechas
+    //         // if ($request->has('FechaInicio') && $request->has('FechaFin')) {
+    //         //     $query->whereBetween('Fecha', [$request->FechaInicio, $request->FechaFin]);
+    //         // }
+
+    //         $horarios = $query->get();
+
+    //         return response()->json($horarios, 200);
+    //     } catch (\Exception $e) {
+    //         return response()->json([
+    //             'msg' => 'Error al listar los horarios.',
+    //             'error' => $e->getMessage()
+    //         ], 500);
+    //     }
+    // }
+
+    public function registrarHorario(Request $request)
+    {
+
+        try {
             $validarHorario = DB::table('horario')
-            ->where('CodigoMedico', $request->CodigoMedico)
-            ->where('Vigente', 1)
-            ->whereDate('Fecha', $request->Fecha)
-            ->where(function ($query) use ($request) {
-                $query->whereTime('HoraInicio', '<', $request->HoraFin)
-                      ->whereTime('HoraFin', '>', $request->HoraInicio);
-            })
-            ->first();
-        
+                ->where('CodigoMedico', $request->CodigoMedico)
+                ->where('Vigente', 1)
+                ->whereDate('Fecha', $request->Fecha)
+                ->where(function ($query) use ($request) {
+                    $query->whereTime('HoraInicio', '<', $request->HoraFin)
+                        ->whereTime('HoraFin', '>', $request->HoraInicio);
+                })
+                ->first();
 
-            if($validarHorario){
+
+            if ($validarHorario) {
                 return response()->json(['msg' => 'El horario se cruza con otro ya registrado. Cruce: ' . $validarHorario->HoraInicio . ' - ' . $validarHorario->HoraFin], 500);
             }
 
@@ -117,7 +124,7 @@ class HorarioController extends Controller
             return response()->json([
                 'msg' => 'Horario registrado correctamente.'
             ], 201);
-        }catch(\Exception $e){
+        } catch (\Exception $e) {
             return response()->json([
                 'msg' => 'Error al registrar el horario.',
                 'error' => $e->getMessage()
